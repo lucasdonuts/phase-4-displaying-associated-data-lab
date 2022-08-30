7a00ff0be83c00c95dce2a6f1baa48b51c940c43
Lucas Duncan <lucasdonuts@gmail.com>
2022-08-30 15:34:21 -0500

    commit tree 952e4d3e3031f340cf58a3f0baa2b596f51c82ba

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,6 @@
 class ItemsController < ApplicationController
+  def index
+    items = Item.all
+    render json: items, except: [:created_at, :updated_at], include: :user
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+  def show
+    user = User.find(params[:id])
+    render json: user, except: [:created_at, :updated_at], include: :items
+  end
 end
